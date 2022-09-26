b04562988d79ab425f16117247c62526b02e5e59
MustafovaSemra <mustafovasemra@yahoo.com>
2022-09-26 17:26:13 -0400

    Added routes

--- src/App.vue
+++ src/App.vue
@@ -35,17 +35,45 @@ export default {
     };
   },
   methods: {
-    addTask(task) {
-      this.tasks = [...this.tasks, task];
+    async addTask(task) {
+      const res = await fetch("http://localhost:5000/tasks", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(task),
+      });
+
+      const data = await res.json();
+
+      this.tasks = [...this.tasks, data];
     },
-    deleteTask(id) {
-      if (confirm("Are you sure?"))
-        this.tasks = this.tasks.filter((task) => task.id !== id);
+    async deleteTask(id) {
+      if (confirm("Are you sure?")) {
+        const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+          method: "DELETE",
+        });
+
+        res.status === 200
+          ? (this.tasks = this.tasks.filter((task) => task.id !== id))
+          : alert("Error deleting task!");
+      }
     },
-    toggleReminder(id) {
-      console.log(id, "toggle");
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id);
+      const updateTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+      const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(updateTask),
+      });
+
+      const data = await res.json();
       this.tasks = this.tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       );
     },
     toggleAddTask() {
@@ -59,7 +87,7 @@ export default {
       return data;
     },
     async fetchTask(id) {
-      const response = await fetch(`api/tasks/${id}`);
+      const response = await fetch(`http://localhost:5000/tasks/${id}`);
 
       const data = await response.json();
 
